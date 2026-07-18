1879a25d73953b3172ae8cd9dbcdabccdbf21b7a
taha najafi <mohtajafi@gmail.com>
2026-07-19 00:07:26 +0330

    تمرین عملگرهای زنجیره‌ای
    در پایتون، می‌توان از عملگرهای مقایسه‌ای به صورت زنجیره‌ای (Chaining) استفاده کرد. به عنوان مثال، عبارت a < b <= c معادل دو مقایسه جداگانه است: a < b و b <= c. این امکان را به شما می‌دهد که چندین مقایسه را در یک عبارت واحد انجام دهید.
    
    وظیفه شما: برنامه‌ای بنویسید که سه عدد ورودی دریافت کند و بررسی کند که آیا عدد اول کمتر از عدد دوم و عدد دوم کمتر یا مساوی عدد سوم است یا خیر.
    
    ورودی‌ها:
    
    سه عدد صحیح به نام‌های a, b و c.
    
    خروجی‌ها:
    
    اگر شرط a < b <= c برقرار باشد، خروجی True را نمایش دهید.
    در غیر این صورت، خروجی False را نمایش دهید.
    
    ورودی نمونه:
    
    5
    10
    15
    
    خروجی نمونه :
    
    True
    
    راهنمایی:
    برای ورودی گرفتن از کاربر می‌توانید از دستور زیر استفاده کنید:
    
    a=input()
    
    حال اگر بخواهیم این ورودی همزمان به int تبدیل شود داریم:
    
    a = int(input())
    
    در ادامه با این دستور بیشتر آشنا خواهید شد.

--- learn.py
+++ learn.py
@@ -1,5 +1,5 @@
-SetA = {1, 2, 3, 4}
-SetB = {3, 4, 5, 6}
-u=SetA.union(SetB)
-i=SetA.intersection(SetB)
-print(f'{u}\n{i}')
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a < b <= c)
