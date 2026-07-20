bd935b4f1bdbb1990cb0645b471bd9de4230e4c3
taha najafi <mohtajafi@gmail.com>
2026-07-20 12:41:37 +0330

    فروشگاه مشروط
    یک فروشگاه تخفیف‌های ویژه‌ای برای مشتریان در نظر گرفته است. قوانین تخفیف به شرح زیر است:
    
    اگر مبلغ خرید بیشتر از ۵۰۰۰۰ تومان باشد، تخفیف ۲۰٪ اعمال می‌شود.
    اگر مبلغ خرید بین ۲۰۰۰۰ تا ۵۰۰۰۰ تومان باشد، تخفیف ۱۰٪ اعمال می‌شود.
    اگر مبلغ خرید کمتر از ۲۰۰۰۰ تومان باشد، تخفیفی اعمال نمی‌شود.
    
    برنامه‌ای بنویسید که مبلغ خرید را از کاربر دریافت کند و مقدار مبلغ نهایی را نمایش دهد.
    ورودی ۱:
    
    55000
    
    خروجی ۱:
    
    44000

--- learn.py
+++ learn.py
@@ -1,5 +1,9 @@
-a = int(input())
-b = int(input())
-c = int(input())
-
-print(a < b <= c)
+mablagh=int(input('mablagh kharid ro begoo: '))
+if mablagh>50000:
+    mablagh_ba_off = int(mablagh-((20*mablagh)/100))
+    print(mablagh_ba_off)
+elif 20000<mablagh<50000 :
+    mablagh_ba_off = int(mablagh-((10*mablagh)/100))
+    print(mablagh_ba_off)
+else:
+    print(mablagh)
